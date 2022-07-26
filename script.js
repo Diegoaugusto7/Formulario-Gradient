067333680e1064238c760e56184d41ed892c1075
Diegoaugusto7 <diegoabdsantos@gmail.com>
2022-07-25 23:28:17 -0300

    Primeiro forms finalizado usando HTML e CSS, copiando layout disponivel na internet

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function Click(){
+    alert('Credenciais Invalidas')
+}
+
+
+function ForgotPW(){
+    alert(`Para recuperar sua senha acesse: ${google}`)
+}
+
+const google = 'https://www.google.com.br'
+
